c7d7a05e5bdcfb2d7426e349461650e66ebf3e2c
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2024-01-18 09:10:34 -0500

    Initial Commit

---
+++ OneWord/OneWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneWordApp.swift
+//  OneWord
+//
+//  Created by Cory Tripathy on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct OneWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
