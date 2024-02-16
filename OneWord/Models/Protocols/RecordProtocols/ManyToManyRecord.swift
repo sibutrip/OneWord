95de584d207afd2343bed7ad5eedaea6dfa7b889
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2024-02-16 16:12:53 -0500

    removed unused protocol recordfetchedbyid

--- OneWord/Models/Protocols/RecordProtocols/ManyToManyRecord.swift
+++ OneWord/Models/Protocols/RecordProtocols/ManyToManyRecord.swift
@@ -5,8 +5,6 @@
 //  Created by Cory Tripathy on 1/25/24.
 //
 
-import Foundation
-
 protocol ManyToManyRecord: FetchedRecord { 
     associatedtype RelatedRecord: ManyToManyRecord
     associatedtype Child: TwoParentsChildRecord
