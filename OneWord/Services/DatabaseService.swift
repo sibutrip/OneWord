a6c741d3950802c36fff0443789e986a13e67780
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2024-03-08 10:04:26 -0500

    removed comments

--- OneWord/Services/DatabaseService.swift
+++ OneWord/Services/DatabaseService.swift
@@ -95,81 +95,10 @@ actor DatabaseService: DatabaseServiceProtocol {
     func newestChildRecord<SomeRecord>(of parent: SomeRecord.Parent) async throws -> SomeRecord where SomeRecord : ChildRecord {
         fatalError("not yet implemented")
     }
-    
-    // MARK: Helper Methods
-    
+        
     // MARK: Initializer
     
     init(withContainer container: CloudContainer) {
         self.container = container
     }
 }
-
-
-//    func fetchManyToManyRecords<FromRecord>(from: FromRecord) -> [FromRecord.RelatedRecord] where FromRecord : ManyToManyRecord {
-//        fatalError("not yet implemented")
-//    }
-
-//    func childRecords<Child>(of secondParent: Child.SecondParent) async throws -> [Child] where Child : TwoParentsChildRecord {
-//        fatalError("not yet implemented")
-//    }
-
-//    func add<Child>(_ record: Child, withParent parent: Child.Parent, andSecondParent secondParent: Child.SecondParent) async throws -> Child where Child : TwoParentsChildRecord, Child.Parent: Record, Child.SecondParent: Record {
-//        var record = record
-//        record.addingParent(parent)
-//        record.addingSecondParent(secondParent)
-//        try await modifyOrAdd([record, parent, secondParent])
-//        return record
-//    }
-
-//    func childRecords<Child>(of parent: Child.Parent) async throws -> [Child] where Child : ChildRecord{
-//        fatalError("not yet implemented")
-//    }
-
-/// - Parameters:
-///   - record: `Child` record to upload to database. Does not need to have its parent property set.
-///   - parent: `Parent` record to upload/update in the database. sets the `parent` property of the corresponding `Child` record
-/// - Returns: `Child` record with updated `parent` property.
-/// - Throws `CloudKitServiceError.couldNotConnectToDatabase` if no internet connection to database.
-/// - `Child` and `Parent` can be existing records, or not. With update or add to database accordingly.
-//    func add<Child>(_ record: Child, withParent parent: Child.Parent) async throws -> Child where Child : ChildRecord, Child.Parent: Record {
-//        var record = record
-//        record.addingParent(parent)
-//        try await modifyOrAdd([record, parent])
-//        return record
-//    }
-
-/// - Throws `CloudKitServiceError.recordNotInDatabase` if no record with given `recordID` exists.
-/// - Throws `CloudKitServiceError.incorrectlyReadingCloudKitData` if `Record` initializer fails with fetched data. Indicates programmer error.
-//    func fetch<SomeRecord>(withID recordID: String) async throws -> SomeRecord where SomeRecord : Record {
-//        guard let ckRecord = try? await database.record(for: CKRecord.ID(recordName: recordID)) else {
-//            throw CloudKitServiceError.recordNotInDatabase
-//        }
-//        guard let record = SomeRecord(from: ckRecord) else {
-//            throw CloudKitServiceError.incorrectlyReadingCloudKitData
-//        }
-//        return record
-//    }
-
-/// - Throws `CloudKitServiceError.couldNotConnectToDatabase` if unable to fetch records in `database`
-/// - Throws `CloudKitServiceError.incorrectlyReadingCloudKitData` if `Record` initializer fails with fetched data. Indicates programmer error.
-//    func newestChildRecord<Child>(of parent: Child.Parent) async throws -> Child where Child : ChildRecord {
-//        let query = CKQuery(recordType: Child.recordType, predicate: NSPredicate(value: true))
-//        query.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-//        guard let (matchResults, _) = (try? await database.records(matching: query, inZoneWith: .default, desiredKeys: nil, resultsLimit: 1)) else {
-//            throw CloudKitServiceError.couldNotConnectToDatabase
-//        }
-//        let records = matchResults
-//            .compactMap { try? $0.1.get() }
-//            .compactMap { Child(from: $0, with: parent) }
-//
-//        guard let record = records.first else {
-//            throw CloudKitServiceError.recordNotInDatabase
-//        }
-//        guard records.count == 1 else {
-//            throw CloudKitServiceError.incorrectlyReadingCloudKitData
-//        }
-//        return record
-//    }
-
-
