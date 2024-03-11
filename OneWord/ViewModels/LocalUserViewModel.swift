2f573d2dbcb124bcccb5fedd57487561db9ec0ac
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2024-03-11 10:51:43 -0400

    added test and prod file

---
+++ OneWord/ViewModels/LocalUserViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  LocalUserViewModel.swift
+//  OneWord
+//
+//  Created by Cory Tripathy on 3/11/24.
+//
+
+class LocalUserViewModel {
+    
+}
---
+++ OneWordTests/LocalUserVMTests.swift
@@ -0,0 +1,14 @@
+//
+//  LocalUserVMTests.swift
+//  OneWordTests
+//
+//  Created by Cory Tripathy on 3/11/24.
+//
+
+import XCTest
+@testable import OneWord
+
+final class LocalUserVMTests: XCTestCase {
+    
+}
+    
